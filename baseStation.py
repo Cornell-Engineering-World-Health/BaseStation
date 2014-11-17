b371e7c37e1baaee4db235eb56c9fe5fb6707f5c
peakyyuter <zackbryant@dhcp-rhodes-1204.redrover.cornell.edu>
2014-11-17 14:47:01 -0500

    Updated Assignment Specification
    need to make database class thread safe and fill nurseHandler thread
    methods

--- baseStation.py
+++ baseStation.py
@@ -1,7 +1,8 @@
 from threading import Thread, Lock, Condition
 import shelve
+import os
 
-#TODO: make this class multi-thread safe
+#TODO: make this class thread safe
 class Database(object):
     def __init__(self,filename):
         self.lock = Lock()
@@ -9,7 +10,22 @@ class Database(object):
 
     #saves database dict in filename
     def saveDatabase(self):
-        self.database.sync()
+        with self.lock:
+            self.database.sync()
+
+    #need to complete
+    def write(self, key, value):
+        pass
+
+    #need to complete
+    def read(self, key):
+        pass
+
+    #need to complete
+    def inDatabase(self, key):
+        pass
+        
+
 
 #TODO: make multi-thread safe + write: formatText, isNegStatusChange, saveInfo
 class VitalDataHandler(Thread):
@@ -23,39 +39,50 @@ class VitalDataHandler(Thread):
     def intStatus(self,status):
         return {'stable':1,'intermediate':0,'critical':-1}[status]
 
+    #need to complete
     #Formats and returns the the text that will be sent out
     #inputs: String patientId, String location, String status
     def formatText(self,patientId,location,status):
         pass
 
+    #need to complete
     #checks database.db if there is a negative status change in the patient
     #returns false if the patient does not exist in the database
     #possible statuses: "stable","intermediate","critical"
     def isNegStatusChange(self,patientId,location,status):
         pass
 
+    #need to complete
     #sends text from pi to nurses
     def sendText(self,msg):
         pass
 
+    #need to complete
     #saves all info in a database.db
-    #KEY: patientId+location; VAL: status
+    #KEY: patientId+','+location; VAL: status
     def saveInfo(self,patientId,location,status):
         pass
-
+        
+    #need to complete
     #thread's main function
     def run(self):
         pass
+ 
+        
+
+       
 
 #TODO: make thread safe + addPhoneNum
 class NurseHandler(Thread):
     def __init__(self,nurseDb):
         self.nurseDb = nurseDb
 
+    #need to complete
     #adds KEY:name, VAL:num to nurseDb and saves to nurseDb
     def addPhoneNum(self,name,num):
         pass
 
+    #need to complete
     #wait for external input (commandline text), then add to nurseDb
     def run(self):
         pass
@@ -70,4 +97,5 @@ if __name__ == "__main__":
     dataInputStream = ["p1,bed1,stable","p2,bed5,critical","p3,bed6,intermediate","p1,bed1,critical","p2,bed5,stable","p3,bed6,intermediate","p1,bed1,critical","p2,bed5,intermediate","p3,bed6,critical"]
     NurseHandler(nurseDb).start()
     for ii in range(9):
-        VitalDataHandler(patientDb,nurseDb,dataInputStream[ii],ii).start()
+        vdh = VitalDataHandler(patientDb,nurseDb,dataInputStream[ii],ii).start()
+        vdh.join()
